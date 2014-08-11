21d477d726cea5fdc34218adf1c35584e8bc3d1f
Katie Mays <komays@phar-storm.phar.umich.edu>
2014-08-11 15:50:55 -0400

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Penguin.m
@@ -0,0 +1,25 @@
+//
+//  Penguin.m
+//  PeevedPenguins
+//
+//  Created by Katie Mays on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Penguin.h"
+
+@implementation Penguin
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
---
+++ PeevedPenguins.spritebuilder/Seal.m
@@ -0,0 +1,26 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Katie Mays on 8/11/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self)
+    {
+        CCLOG(@"Seal created");
+    }
+    
+    return self;
+}
+
+@end
